0a4f0908fe88428c973edcd8efb737016b87c809
Ferhan <mfpop777@gmail.com>
2021-11-24 02:22:02 +0530

    Student insertion with image upload

--- addStudent.php
+++ addStudent.php
@@ -1,20 +1,70 @@
-<?php require_once 'includes/header.php' ; ?>
+<?php 
+require_once 'includes/header.php' ; 
+include_once 'includes/db_connection.php';
+?>
 <?php
+ $upload_errors = '';
  /*Form processing*/
   if (isset($_POST['submit'])) {
-  	print_r($_POST);
+       //print_r($_FILES); die();
+       
+  	$fullname = $_POST['fullname'];
+       $dob =$_POST['dob'];
+       $gender = $_POST['gender'];
+       $phone = $_POST['phone'];
+       $email = $_POST['email'];
+       $country = $_POST['country'];
+       $city = $_POST['city'];
+       //$courseID = $_POST['course'];
+       $course = 'course1';
+
+       //process file upload
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES['photo_upload']['name']);
+        $image_type = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        $upload_ok = false;
+        $photo = $_FILES['photo_upload']['name'];
+
+        //check the file type
+        $check = getimagesize($_FILES['photo_upload']['tmp_name']);
+        if($check == false){
+              echo "file is not an image";
+        }elseif($image_type != "jpg" && $image_type != "png" && $image_type != "jpeg"){
+            echo "only JPG,PNG and JPEG is allowed";  
+        }else{
+              $upload_ok = true;
+        }
+
+        if($upload_ok==true){
+         move_uploaded_file($_FILES["photo_upload"]["tmp_name"], $target_file);
+        }
+
+        //inserting to database and then uploading the file
+
+        $sql = "INSERT INTO students(fullname,dob,gender,phone,email,country_id,city,course,photo) VALUES('$fullname','$dob','$gender','$phone','$email',$country,'$city','$course','$photo') ";
+       if(mysqli_query($dbc,$sql)){
+              $message= "new student added successfully";
+       }else{
+              echo "Error adding new student <br> ".$sql."<br>".mysqli_error($dbc);
+       }       
+
+       
+
+       
   }
 ?>
  <div class="container">
  	<h1>Add new student</h1>
  	<a href="index.php">Home</a>
- 	<form action="addStudent.php" method="POST">
+       <p><?php if (isset($upload_errors)) { echo $upload_errors; } ?></p>
+       <p><?php if (isset($message)) { echo $message; } ?></p>
+ 	<form action="addStudent.php" method="POST" enctype="multipart/form-data">
  		
  		<label for="fullname" class="form-label"> Enter the Full Name :  </label>
- 		 <input type="text" name="fullname" placeholder="Full Name" class="form-control" required>
+ 		 <input type="text" name="fullname" placeholder="Full Name" class="form-control" >
 
  		<label for="dob" class="form-label"> Date of Birth :  </label>
- 		 <input type="date" name="dob" class="form-control" required>
+ 		 <input type="date" name="dob" class="form-control" >
 
  		<label for="gender" class="form-label"> select your Gender :   </label>
  		<p>Male<input type="radio" name="gender" value="male"></p>
@@ -22,22 +72,33 @@
  		<p>Others<input type="radio" name="gender"value="Others"></p>
 
  		<label for="phone" class="form-label">Enter the telephone number : </label>
- 		<input type="tel" name="mobile" pattern="[1-9]{1}[0-9]{9}" title="Enter 10 digit mobile number" required placeholder="Mobile number" class="form-control">
+ 		<input type="tel" name="phone" pattern="[1-9]{1}[0-9]{9}" title="Enter 10 digit mobile number"  placeholder="Mobile number" class="form-control">
               
               <label for="email" class="form-label">Enter the Email : </label>
- 		<input type="email" name="email" placeholder="Email" class="form-control" required>
+ 		<input type="email" name="email" placeholder="Email" class="form-control" >
 
  	       <label for="nationality" class="form-label">Nationality : </label>
-              <select name="nationality" class="form-control">
-                     <option selected>None</option>
-                     <option value="nation1">nation 1</option>
-                     <option value="nation2">nation 2</option>
-                     <option value="nation3">nation 3</option>
-                     <option value="nation3">nation 4</option>
+              <select name="country" class="form-control" >
+              <?php
+               //populating a dropdown with country name
+               $sql = "SELECT * FROM countries";
+               if($result = $dbc->query($sql)){
+                     if(mysqli_num_rows($result)>0){
+                            while($row = mysqli_fetch_array($result)){
+                                   $countryID = $row['countryID'];
+                                   $countryName = $row['countryName'];
+
+                                   //echo "<select name ='select'>";
+                                   echo "<option value=".$countryID.">".$countryName."</option>";
+                                   //echo "</select>";
+                            }
+                     }
+               }
+              ?>
               </select>
 
               <label for="city" class="form-label">Choose City : </label>
-              <select name="city" class="form-control" required>
+              <select name="city" class="form-control" >
  			<option selected>None</option>
  			<option value="city1">city 1</option>
  			<option value="city2">city 2</option>
@@ -47,12 +108,17 @@
 
  		
  	 <label for="courses" class="form-label">Choose desired courses : </label>
-        <p><input type="checkbox" name="course[]" value="course1"> Course 1<p>
-        <p><input type="checkbox" name="course[]" value="course2"> Course 2<p>
-        <p><input type="checkbox" name="course[]" value="course3"> Course 3<p>	
-        
+        <p>
+              <!-- Populating checkbox from the database-->
+              <?php
+
+              ?>
+         <input type="checkbox" name="course[]" value="course1"> Course 1
+         <p>
+        	
+
         <label for="photo" class="form-label">Upload your photo</label>
-        <input type="file" name="photo" class="form-control">
+        <input type="file" name="photo_upload" class="form-control">
 
         <p>
         	<input type="submit" name="submit" value="Add Student">
--- listStudents.php
+++ listStudents.php
@@ -1,6 +1,81 @@
-<?php require_once 'includes/header.php' ; ?>
+<?php 
+require_once 'includes/header.php' ;
+require_once 'includes/db_connection.php' 
+?>
+
  <div class="container">
  	<h1>All students are here</h1>
  	<a href="index.php">Home</a>
+
+    <table class="table table-success table-striped">
+        <thead>
+            <tr>
+                <thead>
+                    <th>StudentId</th>
+                    <th>Full Name</th>
+                    <th>DOB</th>
+                    <th>Gender</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th>Nationality</th>
+                    <th>City</th>
+                    <th>Courses</th>
+                    <th>Photo</th>
+                    <th>Edit Student</th>
+                    <th>Delete Student</th>
+                </thead>
+            </tr>
+        </thead>
+        <tbody class = table-hover>
+        <?php 
+         // Populating the table
+          $sql = "SELECT * FROM students ";
+          if($result = $dbc->query($sql)){
+            if(mysqli_num_rows($result)>0){
+                while($row = mysqli_fetch_array($result)){
+
+                    $studentID = $row['studentID'];
+                    $fullname = $row['fullname'];
+                    $dob = $row['dob'];
+                    $gender = $row['gender'];
+                    $phone = $row['phone'];
+                    $email = $row['email'];
+                    $country_id = $row['country_id'];
+                    $city = $row['city'];
+                    $course = $row['course'];
+                    $photo = $row['photo'];
+                    echo "<tr>";
+                    echo "<td>{$studentID}</td>";
+                    echo "<td>{$fullname}</td>";
+                    echo "<td>{$dob}</td>";
+                    echo "<td>{$gender}</td>";
+                    echo "<td>{$phone}</td>";
+                    echo "<td>{$email}</td>";
+                    echo "<td>{$country_id}</td>";
+                    echo "<td>{$city}</td>";
+                    echo "<td>{$course}</td>";
+                    ?>
+
+                    <td><img width=150 height= 150 src="<?php echo "uploads/$photo";?>"></td>";
+
+                    <?php 
+
+                    echo "<td><a class=\"btn btn-success\" href=\"editStudent.php?id=$studentID\">"."Edit"."</a></td>";
+                    ?>
+                    <td><a  href="<?php echo "deleteStudent.php?id=$studentID"?>" class="btn btn btn-danger" onclick="javascript: return confirm('Please confirm deletion');">Delete</a></td>
+                <?php } 
+                echo "</tbody></table>";
+                //free result
+                mysqli_free_result($result);
+            }else{
+                echo "no courses found";
+            }
+          }else{
+            echo "database query failed ".mysqli_error($dbc);
+          }
+          mysqli_close($dbc);
+         ?> 
+        </tbody>
+    </table>
  </div>
  <?php require_once 'includes/footer.php' ; ?>
